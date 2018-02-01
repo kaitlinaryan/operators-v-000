04cc4b21345a252c724c4822a28b4bf7d35a6fa0
Kaitlin Ryan <kaitlinaryan1@gmail.com>
2018-02-01 18:07:40 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -3,7 +3,7 @@ def unsafe?(speed)
     return true
   elsif speed < 40
     return true
-  else 
+  else
     return false
 end
 end
@@ -11,5 +11,6 @@ end
 
 
 def not_safe?(speed)
-
+if speed > 60 ? true : false
+elsif speed < 40 ? true : false
 end
